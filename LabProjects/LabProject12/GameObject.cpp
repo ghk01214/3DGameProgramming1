6879a0c08abc5395b1f6caf5a98d7292937fb98a
ghk01214 <ghk01214@naver.com>
2021-05-29 20:06:02 +0900

    LabProject12
    Instancing

---
+++ LabProjects/LabProject12/GameObject.cpp
@@ -0,0 +1,194 @@
+#include "framework.h"
+#include "GameObject.h"
+
+CGameObject::CGameObject()
+{
+	XMStoreFloat4x4(&m_xmf4x4World, XMMatrixIdentity());
+}
+
+CGameObject::~CGameObject()
+{
+	if (m_pMesh)
+		m_pMesh->Release();
+
+	if (m_pShader)
+	{
+		m_pShader->ReleaseShaderVariables();
+		m_pShader->Release();
+	}
+}
+
+void CGameObject::Release()
+{
+	if (--m_nReferences <= 0)
+		delete this;
+}
+
+void CGameObject::ReleaseUploadBuffers()
+{
+	// Vertex 버퍼를 위한 업로드 버퍼 소멸
+	if (m_pMesh)
+		m_pMesh->ReleaseUploadBuffers();
+}
+
+void CGameObject::SetMesh(CMesh* pMesh)
+{
+	if (m_pMesh)
+		m_pMesh->Release();
+
+	m_pMesh = pMesh;
+
+	if (m_pMesh)
+		m_pMesh->AddReference();
+}
+
+void CGameObject::SetShader(CShader* pShader)
+{
+	if (m_pShader)
+		m_pShader->Release();
+
+	m_pShader = pShader;
+
+	if (m_pShader)
+		m_pShader->AddReference();
+}
+
+void CGameObject::CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList)
+{
+}
+
+void CGameObject::UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList)
+{
+	XMFLOAT4X4 xmf4x4World;
+
+	XMStoreFloat4x4(&xmf4x4World, XMMatrixTranspose(XMLoadFloat4x4(&m_xmf4x4World)));
+	
+	// 객체의 월드 변환 행렬을 Root 상수(32비트 값)를 통하여 Shader 변수(상수 버퍼)로 복사
+	pd3dCommandList->SetGraphicsRoot32BitConstants(0, 16, &xmf4x4World, 0);
+}
+
+void CGameObject::ReleaseShaderVariables()
+{
+}
+
+XMFLOAT3 CGameObject::GetPosition()
+{
+	return XMFLOAT3(m_xmf4x4World._41, m_xmf4x4World._42, m_xmf4x4World._43);
+}
+
+XMFLOAT3 CGameObject::GetLook()
+{
+	return XMFLOAT3(Vector3::Normalize(XMFLOAT3(m_xmf4x4World._31, m_xmf4x4World._32, m_xmf4x4World._33)));
+}
+
+XMFLOAT3 CGameObject::GetUp()
+{
+	return XMFLOAT3(Vector3::Normalize(XMFLOAT3(m_xmf4x4World._21, m_xmf4x4World._22, m_xmf4x4World._23)));
+}
+
+XMFLOAT3 CGameObject::GetRight()
+{
+	return XMFLOAT3(Vector3::Normalize(XMFLOAT3(m_xmf4x4World._11, m_xmf4x4World._12, m_xmf4x4World._13)));
+}
+
+void CGameObject::SetPosition(FLOAT x, FLOAT y, FLOAT z)
+{
+	m_xmf4x4World._41 = x;
+	m_xmf4x4World._42 = y;
+	m_xmf4x4World._43 = z;
+}
+
+void CGameObject::SetPosition(XMFLOAT3 xmf3Position)
+{
+	SetPosition(xmf3Position.x, xmf3Position.y, xmf3Position.z);
+}
+
+void CGameObject::MoveStrate(FLOAT fDistance)
+{
+	XMFLOAT3 xmf3Position{ GetPosition() };
+	XMFLOAT3 xmf3Right{ GetRight() };
+	
+	xmf3Position = Vector3::Add(xmf3Position, xmf3Right, fDistance);
+	
+	CGameObject::SetPosition(xmf3Position);
+}
+
+void CGameObject::MoveUp(FLOAT fDistance)
+{
+	XMFLOAT3 xmf3Position{ GetPosition() };
+	XMFLOAT3 xmf3Up{ GetUp() };
+
+	xmf3Position = Vector3::Add(xmf3Position, xmf3Up, fDistance);
+
+	CGameObject::SetPosition(xmf3Position);
+}
+
+void CGameObject::MoveForward(FLOAT fDistance)
+{
+	XMFLOAT3 xmf3Position = GetPosition();
+	XMFLOAT3 xmf3Look = GetLook();
+
+	xmf3Position = Vector3::Add(xmf3Position, xmf3Look, fDistance);
+
+	CGameObject::SetPosition(xmf3Position);
+}
+
+void CGameObject::Rotate(FLOAT fPitch, FLOAT fYaw, FLOAT fRoll)
+{
+	XMMATRIX mtxRotate{ XMMatrixRotationRollPitchYaw(XMConvertToRadians(fPitch), XMConvertToRadians(fYaw), XMConvertToRadians(fRoll)) };
+	
+	m_xmf4x4World = Matrix4x4::Multiply(mtxRotate, m_xmf4x4World);
+}
+
+void CGameObject::Rotate(XMFLOAT3* pxmf3Axis, FLOAT fAngle)
+{
+	XMMATRIX mtxRotate{ XMMatrixRotationAxis(XMLoadFloat3(pxmf3Axis), XMConvertToRadians(fAngle)) };
+
+	m_xmf4x4World = Matrix4x4::Multiply(mtxRotate, m_xmf4x4World);
+}
+
+void CGameObject::Animate(FLOAT fTimeElapsed)
+{
+}
+
+void CGameObject::PrepareRender()
+{
+}
+
+void CGameObject::Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera)
+{
+	PrepareRender();
+
+	// 객체의 정보를 Shader 변수(상수 버퍼)로 복사
+	UpdateShaderVariables(pd3dCommandList);
+
+	if (m_pShader)
+		m_pShader->Render(pd3dCommandList, pCamera);
+
+	if (m_pMesh)
+		m_pMesh->Render(pd3dCommandList);
+}
+
+// Instancing Vertex 뷰를 사용하여 Mesh Rendering
+void CGameObject::Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera, UINT nInstances, D3D12_VERTEX_BUFFER_VIEW d3dInstancingBufferView)
+{
+	PrepareRender();
+
+	if (m_pMesh)
+		m_pMesh->Render(pd3dCommandList, nInstances, d3dInstancingBufferView);
+}
+
+CRotatingObject::CRotatingObject()
+{
+	m_xmf3RotationAxis	 = XMFLOAT3(0.0f, 1.0f, 0.0f);
+	m_fRotationSpeed	 = 90.0f;
+}
+
+CRotatingObject::~CRotatingObject()
+{
+}
+
+void CRotatingObject::Animate(FLOAT fTimeElapsed)
+{
+	CGameObject::Rotate(&m_xmf3RotationAxis, m_fRotationSpeed * fTimeElapsed);
+}
---
+++ LabProjects/LabProject12/GameObject.h
@@ -0,0 +1,74 @@
+#pragma once
+#include "Mesh.h"
+#include "Camera.h"
+#include "Shader.h"
+
+class CShader;
+class CCamera;
+
+class CGameObject
+{
+public:
+	CGameObject();
+	virtual ~CGameObject();
+public:
+	void AddReference() { ++m_nReferences; }
+	void Release();
+public:
+	void ReleaseUploadBuffers();
+public:
+	virtual void SetMesh(CMesh* pMesh);
+	virtual void SetShader(CShader* pShader);
+public:
+	virtual void CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);		// 상수 버퍼 생성
+	virtual void UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList);									// 상수 버퍼의 내용 갱신
+	virtual void ReleaseShaderVariables();
+public:
+	// Game Object의 월드 변환 행렬에서 위치 벡터와 방향(x축, y축, z축)벡터 반환
+	XMFLOAT3 GetPosition();
+	XMFLOAT3 GetLook();
+	XMFLOAT3 GetUp();
+	XMFLOAT3 GetRight();
+public:
+	// Game Object의 위치 설정
+	void SetPosition(FLOAT x, FLOAT y, FLOAT z);
+	void SetPosition(XMFLOAT3 xmf3Position);
+public:
+	// Game Object를 Local x축, y축, z축 방향으로 이동
+	void MoveStrate(FLOAT fDistance = 1.0f);
+	void MoveUp(FLOAT fDistance = 1.0f);
+	void MoveForward(FLOAT fDistance = 1.0f);
+public:
+	// Game Object를 회전(x축, y축, z축)
+	void Rotate(FLOAT fPitch = 10.0f, FLOAT fYaw = 10.0f, FLOAT fRoll = 10.0f);
+	void Rotate(XMFLOAT3* pxmf3Axis, FLOAT fAngle);
+public:
+	virtual void Animate(FLOAT fTimeElapsed);
+public:
+	virtual void PrepareRender();
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera);
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera, UINT nInstances, D3D12_VERTEX_BUFFER_VIEW d3dInstancingBufferView);
+
+private:
+	INT					m_nReferences{ 0 };
+protected:
+	XMFLOAT4X4			m_xmf4x4World;
+	CMesh*				m_pMesh{ nullptr };
+	CShader*			m_pShader{ nullptr };
+};
+
+class CRotatingObject : public CGameObject
+{
+public:
+	CRotatingObject();
+	virtual ~CRotatingObject();
+public:
+	void SetRotationSpeed(FLOAT fRotationSpeed) { m_fRotationSpeed = fRotationSpeed; }
+	void SetRotationAxis(XMFLOAT3 xmf3RotationAxis) { m_xmf3RotationAxis = xmf3RotationAxis; }
+public:
+	virtual void Animate(FLOAT fTimeElapsed);
+
+private:
+	XMFLOAT3			 m_xmf3RotationAxis;
+	FLOAT				 m_fRotationSpeed;
+};
