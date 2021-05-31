44b4f4da24490b3ab26dcb69921bd13bc7cf2927
ghk01214 <ghk01214@naver.com>
2021-06-01 00:00:42 +0900

    LabProject13
    Instancing(안 그려짐, 추후 수정 필요)

---
+++ LabProjects/LabProject13/Mesh.h
@@ -0,0 +1,80 @@
+#pragma once
+
+class CVertex
+{
+public:
+	CVertex() { m_xmf3Position = XMFLOAT3(0.0f, 0.0f, 0.0f); }
+	CVertex(XMFLOAT3 xmf3Position) { m_xmf3Position = xmf3Position; }
+	~CVertex() {}
+protected:
+	XMFLOAT3 m_xmf3Position;														// 정점의 위치 벡터(모든 정점은 최소한 위치 벡터를 가져야 한다)
+};
+
+class CDiffusedVertex : public CVertex
+{
+public:
+	CDiffusedVertex();
+	CDiffusedVertex(FLOAT x, FLOAT y, FLOAT z, XMFLOAT4 xmf4Diffuse);
+	CDiffusedVertex(XMFLOAT3 xmf3Position, XMFLOAT4 xmf4Diffuse);
+	~CDiffusedVertex() {}
+protected:
+	XMFLOAT4 m_xmf4Diffuse;															// 정점의 색상
+};
+
+class CMesh
+{
+public:
+	CMesh() {}
+	CMesh(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+	virtual ~CMesh();
+public:
+	void AddReference() { ++m_nReferences; }
+	void Release();
+	void ReleaseUploadBuffers();
+public:
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, UINT nInstances = 1);
+
+private:
+	INT							 m_nReferences{ 0 };
+protected:
+	ID3D12Resource*				 m_pd3dVertexBuffer{ nullptr };
+	ID3D12Resource*				 m_pd3dVertexUploadBuffer{ nullptr };
+protected:
+	D3D12_VERTEX_BUFFER_VIEW	 m_d3dVertexBufferView;
+protected:
+	D3D12_PRIMITIVE_TOPOLOGY	 m_d3dPrimitiveTopology = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	UINT						 m_nSlot{ 0 };
+	UINT						 m_nVertices{ 0 };
+	UINT						 m_nStride{ 0 };
+	UINT						 m_nOffset{ 0 };
+protected:
+	ID3D12Resource*				 m_pd3dIndexBuffer{ nullptr };						// Index 버퍼 Interface 포인터(Vertex 버퍼(배열)에 대한 Index를 가짐)
+	ID3D12Resource*				 m_pd3dIndexUploadBuffer{ nullptr };				// Index 버퍼 업로드 버퍼 Interface 포인터
+	D3D12_INDEX_BUFFER_VIEW		 m_d3dIndexBufferView;
+protected:
+	UINT						 m_nIndices{ 0 };									//인덱스 버퍼에 포함되는 인덱스의 개수이다.
+	UINT						 m_nStartIndex{ 0 };								//인덱스 버퍼에서 메쉬를 그리기 위해 사용되는 시작 인덱스이다.
+	INT							 m_nBaseVertex{ 0 };								//인덱스 버퍼의 인덱스에 더해질 인덱스이다.
+};
+
+class CTriangleMesh : public CMesh
+{
+public:
+	CTriangleMesh(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+	virtual ~CTriangleMesh() {}
+};
+
+class CCubeMeshDiffused : public CMesh
+{
+public:
+	// 직육면체의 가로, 세로, 높이의 길이를 지정하여 직육면체 Mesh 생성
+	CCubeMeshDiffused(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, FLOAT fWidth, FLOAT fHeight, FLOAT fDepth);
+	virtual ~CCubeMeshDiffused();
+};
+
+class CAirplaneMeshDiffused : public CMesh
+{
+public:
+	CAirplaneMeshDiffused(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, FLOAT fWidth = 20.0f, FLOAT fHeight = 20.0f, FLOAT fDepth = 4.0f, XMFLOAT4 xmf4Color = XMFLOAT4(1.0f, 1.0f, 0.0f, 0.0f));
+	virtual ~CAirplaneMeshDiffused();
+};
---
+++ LabProjects/LabProject13/VertexShader.hlsl
@@ -0,0 +1,69 @@
+// Game Object의 정보를 위한 상수 버퍼 선언
+cbuffer cbGameObjectInfo : register(b0)
+{
+	matrix gmtxWorld : packoffset(c0);
+};
+
+// 카메라의 정보를 위한 상수 버퍼 선언
+cbuffer cbCameraInfo : register(b1)
+{
+	matrix gmtxView : packoffset(c0);
+	matrix gmtxProjection : packoffset(c4);
+};
+
+// Vertex Shader의 입력을 위한 구조체 선언
+struct VS_INPUT
+{
+	float3 fPosition : POSITION;
+	float4 fColor : COLOR;
+};
+
+// Vertex Shader의 출력(Pixel Shader의 입력)을 위한 구조체 선언
+struct VS_OUTPUT
+{
+	float4 fPosition : SV_POSITION;
+	float4 fColor : COLOR;
+};
+
+// Instancing 데이터를 위한 구조체
+struct INSTANCEDGAMEOBJECTINFO
+{
+	matrix m_mtxGameObject;
+	float4 m_cColor;
+};
+
+StructuredBuffer<INSTANCEDGAMEOBJECTINFO> gGameObjectInfos : register(t0);
+
+struct VS_INSTANCING_INPUT
+{
+	float3 fPosition : POSITION;
+	float4 fColor : COLOR;
+};
+
+struct VS_INSTANCING_OUTPUT
+{
+	float4 fPosition : SV_POSITION;
+	float4 fColor : COLOR;
+};
+
+// Vertex Shader 정의
+VS_OUTPUT Diffused(VS_INPUT vInput)
+{
+	VS_OUTPUT vOutput;
+
+	// Vertex를 변환(월드, 카메라, 투영)
+	vOutput.fPosition = mul(mul(mul(float4(vInput.fPosition, 1.0f), gmtxWorld), gmtxView), gmtxProjection);
+	vOutput.fColor = vInput.fColor;
+
+	return vOutput;
+}
+
+VS_INSTANCING_OUTPUT Instancing(VS_INSTANCING_INPUT vInput, uint nInstanceID : SV_InstanceID)
+{
+	VS_INSTANCING_OUTPUT vOutput;
+
+	vOutput.fPosition = mul(mul(mul(float4(vInput.fPosition, 1.0f), gGameObjectInfos[nInstanceID].m_mtxGameObject), gmtxView), gmtxProjection);
+	vOutput.fColor = vInput.fColor + gGameObjectInfos[nInstanceID].m_cColor;
+
+	return vOutput;
+}
