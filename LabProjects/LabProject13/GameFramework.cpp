2fc6db50234b92482c5a6a07f0c0d657c20c8c40
ghk01214 <ghk01214@naver.com>
2021-06-01 02:20:53 +0900

    오타 수정

--- LabProjects/LabProject13/GameFramework.cpp
+++ LabProjects/LabProject13/GameFramework.cpp
@@ -383,7 +383,7 @@ void CGameFramework::BuildObjects()
 	m_pScene = new CScene();
 	m_pScene->BuildObjects(m_pd3dDevice, m_pd3dCommandList);
 
-	CAirplanePlayer* pAirplanePlayer{ new CAirplanePlayer(m_pd3dDevice, m_pd3dCommandList, m_pScene->GetGraphicsRootSignature()) };
+	CAirplanePlayer, * pAirplanePlayer{ new CAirplanePlayer, (m_pd3dDevice, m_pd3dCommandList, m_pScene->GetGraphicsRootSignature()) };
 	m_pPlayer = pAirplanePlayer;
 	m_pCamera = m_pPlayer->GetCamera();
 
